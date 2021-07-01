9ce97d8973e2d34aca04a815111c2a0d84c16173
Liandus <losox@yandex.ru>
2021-07-01 13:05:58 +0300

    Анимация и доработка форм, а также мелкие правки.

--- js/script-letter.js
+++ js/script-letter.js
@@ -1,15 +1,49 @@
 let popupLetter=document.querySelector('.modal-letter');
 let openLetter=document.querySelector('.open-form-button');
 let closeLetter=popupLetter.querySelector('.close-button');
+let form=popupLetter.querySelector('.letter-form');
+let userName=popupLetter.querySelector('.letter-login');
+let userEmail=popupLetter.querySelector('.letter-email');
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 openLetter.addEventListener('click', function(){
-    popupLetter.classList.add('modal--show')
+    popupLetter.classList.add('modal--show-letter')
+    if (storage) {
+      userEmail.value = storage;
+    }
+    userName.focus();
 });
 
 closeLetter.addEventListener('click', function(){
-    popupLetter.classList.remove('modal--show')
+    popupLetter.classList.remove('modal--show-letter')
+    popupLetter.classList.remove('modal--error');
 });
 
 window.addEventListener('keydown', function(evt){
-    if (evt.key=="Escape" || evt.key=="Esc"){popupLetter.classList.remove('modal--show');}
+    if (evt.key=="Escape" || evt.key=="Esc")
+    {popupLetter.classList.remove('modal--show-letter');
+     popupLetter.classList.remove('modal--error');
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!userName.value || !userEmail.value) {
+    evt.preventDefault();
+    popupLetter.classList.remove('modal--error');
+    popupLetter.offsetWidth = popupLetter.offsetWidth;
+    popupLetter.classList.add('modal--error');
+  } else {if(isStorageSupport){
+
+    localStorage.setItem("email",userEmail.value);
+    }
+  }
+
 });
