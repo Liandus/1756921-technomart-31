24802be9445896aafde4c46eff4f51f67ceaa959
Liandus <losox@yandex.ru>
2021-07-10 15:51:17 +0300

    Вторая проверка

--- js/script-countcart.js
+++ js/script-countcart.js
@@ -1,7 +1,10 @@
 let addCarts=document.querySelectorAll('.catalog-button-buy');
 let carts=document.querySelector('.cart');
 let counterC=document.querySelector('.cart-number')
-let count=0;
+let count=counterC.textContent;
+count=Number(count);
+
+
 for (let i = 0; i< addCarts.length; i++) {
   addCart= addCarts[i];
 addCart.addEventListener('click', function (evt) {
